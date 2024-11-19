293857c148a4ffef7b4115a19049bd3b94ecfd97
Curtis Chong <curtischong5@gmail.com>
2024-11-19 14:13:59 -0800

    Simplify tensor product logic (#4)
    * tensor product is much more readable
    
    * tensor product coefficients now match the image
    
    * backup before making data field manditory
    
    * simplified irrep class by requiring data to be set
    
    * irrep_test really explains how tensor products work
    
    * all irreps tests pass
    
    * docs and ruff
    
    * made test_matches_e3nn test

--- clebsch_gordan.py
+++ clebsch_gordan.py
@@ -1,26 +1,18 @@
 from functools import lru_cache
 import e3nn_jax
-# from e3x.so3 import clebsch_gordan
-from e3nn.o3._wigner import _so3_clebsch_gordan
-from e3x.so3._symbolic import _clebsch_gordan
+
+from spherical_harmonics import to_cartesian_order_idx
 
 
 def get_clebsch_gordan(l1: int, l2: int, l3: int, m1: int, m2: int, m3: int) -> float:
-    # cg = _get_clebsch_gordan(l1, l2, l3)
-    res = float(_clebsch_gordan(l1, l2, l3, m1,m2,m3).evalf())
-    return res
-    # print("e3nn cg shape", cg.shape)
-
-    # I'm pretty sure we add each li to mi because mi starts at -li. So we need to offset it by li
-    # return cg[l1 + m1, l2 + m2, l3 + m3]
+    m1_idx = to_cartesian_order_idx(l1, m1)
+    m2_idx = to_cartesian_order_idx(l2, m2)
+    m3_idx = to_cartesian_order_idx(l3, m3)
+    return _get_clebsch_gordan(l1, l2, l3)[m1_idx, m2_idx, m3_idx]
 
 @lru_cache(maxsize=None)
 def _get_clebsch_gordan(l1: int, l2: int, l_out: int) -> str:
-    cg = e3nn_jax.clebsch_gordan(l1, l2, l_out)
-    # print("e3nn cg shape", cg.shape)
-    # cg = _so3_clebsch_gordan(l1, l2, l_out)
-    # cg = clebsch_gordan(l1, l2, l_out, cartesian_order=True)
-    return cg
+    return e3nn_jax.clebsch_gordan(l1, l2, l_out)
 
 if __name__ == "__main__":
     print(e3nn_jax.clebsch_gordan(1, 1, 2))
--- irrep.py
+++ irrep.py
@@ -1,64 +1,81 @@
 from __future__ import annotations
-from collections import defaultdict
 import torch
 import dataclasses
 import re
 
+from clebsch_gordan import get_clebsch_gordan
 from constants import EVEN_PARITY, ODD_PARITY
-from e3nn.o3._wigner import _so3_clebsch_gordan
-from e3nn_jax import clebsch_gordan
-import e3nn_jax
+from spherical_harmonics import to_cartesian_order_idx
 
 
 class Irreps:
-    irreps: list[Irrep]
+    # this class contains a list of irreps.
+    irreps: list[
+        Irrep
+    ]  # this is always sorted from smallest l to largest l and odd parity to even parity
 
     def __init__(self, irreps_list: list[Irrep]):
-        self.irreps = irreps_list
-        # self.irreps = []
-        # irreps_defs = irrep_defs_str.split("+")
-        # irreps_defs = [irrep_def.strip() for irrep_def in irreps_defs]
-        # irreps_pattern = r"^(\d+)x+(\d+)([eo])$"
-        # for irrep_def in irreps_defs:
+        assert irreps_list, "irreps_list must not be empty"
+        self.irreps = sorted(irreps_list, key=lambda irrep: (irrep.l, irrep.parity))
 
-        #     # create irreps from the string
-        #     match = re.match(irreps_pattern, irrep_def)
-        #     if not bool(match):
-        #         raise ValueError(f"irrep_def {irrep_def} is not valid. it need to look something like: 1x1o + 1x2e + 1x3o")
-        #     num_irreps, l_str, parity_str = match.groups()
-        #     parity = -1 if parity_str == "o" else 1
-        #     for _ in range(int(num_irreps)):
-        #         self.irreps.append(Irrep(int(l_str), parity, None))
+    @staticmethod
+    def from_id(id: str, data: list[torch.Tensor]) -> Irreps:
+        irreps_defs = id.split("+")
+        irreps_defs = [irrep_def.strip() for irrep_def in irreps_defs]
+        irreps_pattern = r"^(\d+)x+(\d+)([eo])$"
+
+        data_idx = 0  # advance to the next data when we create the next Irrep object
+        irreps = []
+        for irrep_def in irreps_defs:
+            # create irreps from the string
+            match = re.match(irreps_pattern, irrep_def)
+            if not bool(match):
+                raise ValueError(
+                    f"irrep_def {irrep_def} is not valid. it need to look something like: 1x1o + 1x2e + 1x3o"
+                )
+            num_irreps, l_str, parity_str = match.groups()
+
+            l = int(l_str)
+            parity = ODD_PARITY if parity_str == "o" else EVEN_PARITY
+
+            for _ in range(int(num_irreps)):
+                if data_idx >= len(data):
+                    raise ValueError(
+                        f"not enough data for the irrep {l}x{parity_str}. you need {l} data tensors"
+                    )
+                irreps.append(Irrep(l, parity, data[data_idx]))
+                data_idx += 1
+
+        assert (
+            len(irreps) == len(data)
+        ), f"the number of irreps ({len(irreps)}) must match the number of data tensors ({len(data)})"
+        return Irreps(irreps)
 
     def __repr__(self) -> str:
-        irreps_of_same_l_and_parity = defaultdict(list[Irrep])
-        max_l = 0
-        for irrep in self.irreps:
-            max_l = max(max_l, irrep.l)
-            irreps_of_same_l_and_parity[irrep.id()].append(irrep)
+        return f"{self.id()}: {str(self.data_flattened())}"
 
-        # order the representations by l and parity (so it is easier to read)
-        consolidated_repr = []
-        consolidated_data = []
-        for i in range(0, max_l + 1):
-            for parity in [ODD_PARITY, EVEN_PARITY]:
-                irrep_id = Irrep(i, parity, None).id()
-                if irrep_id not in irreps_of_same_l_and_parity:
-                    continue
-
-                num_irreps_of_id = len(irreps_of_same_l_and_parity[irrep_id])
-                consolidated_repr.append(f"{num_irreps_of_id}x{irrep_id}")
-
-                for irrep in irreps_of_same_l_and_parity[irrep_id]:
-                    if irrep.data is None:
-                        continue
-                    consolidated_data.extend(irrep.data.tolist())
-        consolidated_ids = "+".join(consolidated_repr)
-        return f"{consolidated_ids}: {str(consolidated_data)}"
-
-
+    # tells you how many irreps are in the object
+    # e.g. 1x2o+3x4e means there is:
+    #   1 irrep with l=2 and parity=odd. We need 1*5=5 coefficients to represent this. This is represented as 1 irrep (in the irreps list)
+    #   3 irreps with l=4 and parity=even. We need 3*9=27 coefficients to represent this. This is represented as 3 irreps (in the irreps list)
+    # In total, there are: 32 coefficients spanning across 4 irreps
     def id(self):
-        return self.__repr__()
+        irrep_ids_with_cnt = []
+        current_id = self.irreps[0].id()
+        irrep_of_same_id_cnt = 1
+
+        for irrep in self.irreps[1:]:
+            if irrep.id() == current_id:
+                irrep_of_same_id_cnt += 1
+            else:
+                irrep_ids_with_cnt.append(f"{irrep_of_same_id_cnt}x{current_id}")
+                current_id = irrep.id()
+                irrep_of_same_id_cnt = 1
+
+        # Append the last group
+        irrep_ids_with_cnt.append(f"{irrep_of_same_id_cnt}x{current_id}")
+
+        return "+".join(irrep_ids_with_cnt)
 
     def tensor_product(self, other: Irreps):
         new_irreps = []
@@ -67,101 +84,98 @@ class Irreps:
                 new_irreps.extend(irrep1.tensor_product(irrep2))
         return Irreps(new_irreps)
 
-@dataclasses.dataclass(init=False)
-class Irrep():
-    l: int
-    parity: int
-    
-    data: torch.tensor | None
+    def data(self):
+        return [irrep.data for irrep in self.irreps]
 
-    def __init__(self, l: int, parity: int, data: torch.tensor | None):
+    def data_flattened(self) -> list[float]:
+        consolidated_data = []
+        for irrep in self.irreps:
+            consolidated_data.extend(irrep.data.tolist())
+        return consolidated_data
+
+
+@dataclasses.dataclass(init=False)
+class Irrep:
+    # this class contains the coefficients for 2*l + 1 spherical harmonics of degree l and parity p
+    l: int  # determines: "which order of spherical harmonics we are dealing with". if l=3, this irrep carries the data for spherical harmonics of degree 3. there are 2*3+1 = 7 spherical harmonics of degree 3. so we need 7 coefficients to represent this
+    parity: int  # Note: the parity doesn't determine the NUMBER of coefficients. It's just used to determine the the parity of irreps resulting from tensor products
+    data: torch.Tensor
+
+    def __init__(self, l: int, parity: int, data: torch.Tensor):
         assert l >= 0, "l (the degree of your representation) must be non-negative"
-        assert parity in {EVEN_PARITY, ODD_PARITY}, f"p (the parity of your representation) must be 1 (even) or -1 (odd). You passed in {parity}"
+        assert (
+            parity in {EVEN_PARITY, ODD_PARITY}
+        ), f"p (the parity of your representation) must be 1 (even) or -1 (odd). You passed in {parity}"
+        assert (
+            data.numel() == 2 * l + 1
+        ), f"Expected {2*l + 1} coefficients for l={l}, parity={parity}. Got {data.numel()} coefficients instead"
+        assert (
+            data.dim() == 1
+        ), f"data array passed to irrep is {data.dim}-dimensional. Please make sure it's 1D instead"
         self.l = l
         self.parity = parity
         self.data = data
 
     @staticmethod
-    def from_id(irrep_id: str, data: torch.tensor | None):
+    def from_id(irrep_id: str, data: torch.Tensor):
         irrep_pattern = r"^(\d+)([eo])$"
         match = re.match(irrep_pattern, irrep_id)
         if not bool(match):
-            raise ValueError(f"irrep_id {irrep_id} is not valid. it need to look something like: 1o or 7e. (this is the order l followed by the parity (e or o)")
+            raise ValueError(
+                f"irrep_id {irrep_id} is not valid. it need to look something like: 1o or 7e. (this is the order l followed by the parity (e or o)"
+            )
+
         l_str, parity_str = match.groups()
         parity = -1 if parity_str == "o" else 1
+
         return Irrep(int(l_str), parity, data)
 
-    def get_coefficient(self, m:int) -> float:
-        if self.data is None:
-            raise ValueError(f"data is None when trying to get m={m} coefficient for {self}")
-        return self.data[self._to_cartesian_order_idx(self.l, m, add_offset=False)] # since m starts at -l and goes to l, we need to add an offset to l to get the correct index
+    def get_coefficient(self, m: int) -> float:
+        return self.data[to_cartesian_order_idx(self.l, m)]
 
     def tensor_product(self, irrep2: Irrep) -> list[Irrep]:
         irrep1 = self
-        parity_out = irrep1.parity * irrep2.parity
+
         l1 = irrep1.l
         l2 = irrep2.l
-
         l_min = abs(l1 - l2)
         l_max = l1 + l2
+
+        parity_out = irrep1.parity * irrep2.parity
 
         res_irreps = []
 
         # the tensor product of these two irreps will generate (max_l+1 - min_l) new irreps. where each new irrep has l=l_out and parity=parity_out
         for l_out in range(l_min, l_max + 1):
-            if irrep1.data is None or irrep2.data is None:
-                coefficients = None
-            else:
-                coefficients = [0]*(2*l_out+1)
-                # this irrep has 2l+1 coefficients
-                # we need to calculate it here
-                for m_out in range(-l_out, l_out + 1):
-                    coefficient = 0
-                    m3_idx = self._to_cartesian_order_idx(l_out, m_out, add_offset=False)
-                    for m1 in range(-l1, l1 + 1):
-                        for m2 in range(-l2, l2 + 1):
-                            # cg = _so3_clebsch_gordan(l1, l2, l_out)[l1 + m1, l2 + m2, l_out + m_out] # we add each li to mi because mi starts at -li. So we need to offset it by li
-                            m1_dx = self._to_cartesian_order_idx(l1, m1, add_offset=False)
-                            m2_idx = self._to_cartesian_order_idx(l2, m2, add_offset=False)
-                            # print(f"l1={l1}, l2={l2}, l_out={l_out}, m11={m11}, m22={m22}, m33={m33}")
-                            cg = e3nn_jax.clebsch_gordan(l1, l2, l_out)[m1_dx, m2_idx, m3_idx]
-                            v1 = irrep1.get_coefficient(m1)
-                            v2 = irrep2.get_coefficient(m2)
-                            normalization = 1
-                            coefficient += cg*v1*v2*normalization
-                    coefficients[m3_idx] = coefficient
+            # calculate all 2l+1 coefficients for this irrep here
+            coefficients = [0] * (2 * l_out + 1)
+            for m3 in range(-l_out, l_out + 1):
+                # here we are doing the summation to get the coefficient for this m3 (see assets/tensor_product.png for the formula)
+                coefficient = 0
+                for m1 in range(-l1, l1 + 1):
+                    for m2 in range(-l2, l2 + 1):
+                        cg = get_clebsch_gordan(l1, l2, l_out, m1, m2, m3)
+                        v1 = irrep1.get_coefficient(m1)
+                        v2 = irrep2.get_coefficient(m2)
+                        normalization = 1
+                        coefficient += cg * v1 * v2 * normalization
+
+                m3_idx = to_cartesian_order_idx(
+                    l_out, m3
+                )  # put the coefficient in the right index since we're following Cartesian order convention https://e3x.readthedocs.io/stable/pitfalls.html
+                coefficients[m3_idx] = coefficient
+
                 coefficients = torch.tensor(coefficients)
             res_irreps.append(Irrep(l_out, parity_out, coefficients))
         return res_irreps
-    
-    def _to_cartesian_order_idx(self, l: int, m: int, add_offset: bool = True):
-        # return l + m
-        if add_offset:
-            start_index = l**2
-        else:
-            start_index = 0
 
-        abs_m = abs(m)
-        num_m_in_l = 2*l + 1
-
-        if m == 0:
-            pos = num_m_in_l - 1
-        elif m < 0:
-            pos = num_m_in_l - 1 - 2*abs_m + 1
-        else:
-            pos = num_m_in_l - 1 - 2*abs_m
-        
-        res = start_index + pos
-        # print(f"m={m}, l={l}, res={res}")
-        return res
-
-    
     def __repr__(self) -> str:
         id = self.id()
-        if self.data is None:
-            return f"{id}"
         return f"{id}: {self.data.tolist()}"
 
+    # tells you what type of irrep this is
+    # e.g. 2o means it's for spherical harmonics of degree 2 (l=2) and parity=odd
+    # since l=2, there are 2*2 + 1 = 5 coefficients that this irrep stores in its data array
     def id(self):
         if self.parity == 1:
             parity_str = "e"
