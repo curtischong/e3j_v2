trying to fix loss calculation:

model.py
@@ -21,14 +21,15 @@ class Model(torch.nn.Module):
         self.activation_layer1 = ActivationLayer("GELU", "5x0e + 5x1o")
         self.layer2 = Layer("5x0e + 5x1o", "5x0e + 5x1o")
         self.activation_layer2 = ActivationLayer("GELU", "5x0e + 5x1o")
-        self.layer3 = Layer("5x0e + 5x1o", "1x0e")
+        self.layer3 = Layer("5x0e + 5x1o", "5x0e")
+        self.activation_layer3 = ActivationLayer("GELU", "5x0e + 5x1o")
 
         # intermediate layers
         # self.layer2 = Layer("5x0e + 5x1o", "5x0e + 5x1o")
         # self.layer3 = Layer("5x0e + 5x1o", "5x0e + 5x1o")
 
         # output layer
-        num_scalar_features = 1  # since the output of layer1 is 1x
+        num_scalar_features = 5  # since the output of layer3 is 5x
         self.output_mlp = torch.nn.Linear(
             num_scalar_features, num_classes, dtype=default_dtype
         )
@@ -52,13 +53,14 @@ class Model(torch.nn.Module):
         x = self.layer2(x, edge_index, positions)
         x = self.activation_layer2(x)
         x = self.layer3(x, edge_index, positions)
+        x = self.activation_layer3(x)
         # x = self.layer2(x, edge_index, positions)
         # x = self.layer3(x, edge_index, positions)
 
         # now pool the features on each node to generate the final output irreps
         pooled_feats = avg_irreps_with_same_id(x)
-        scalar_feats = pooled_feats.get_irreps_by_id("0e")[0].data
-        return self.softmax(self.output_mlp(scalar_feats))
+        scalar_feats = [irrep.data for irrep in pooled_feats.get_irreps_by_id("0e")]
+        return self.softmax(self.output_mlp(torch.cat(scalar_feats)))
 
 
 # IMPORTANT: LinearLayer are the weights for an individual node. you re-use it for each different node in the graph
@@ -218,7 +220,7 @@ class Layer(torch.nn.Module):
 
             # Compute tensor product
             tensor_product = dest_node_feat.tensor_product(
-                sh, compute_up_to_l=self.sh_lmax
+                sh, compute_up_to_l=self.sh_lmax, norm_type="component"
             )
             # tensor_product.data()[0].data += self.addition2
 