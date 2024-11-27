5cd919319e65954f01572737b3c710fb40134492
Curtis Chong <curtischong5@gmail.com>
2024-11-26 17:10:24 -0800

    added comment about equivariance tolerance

--- test_equivariance.py
+++ test_equivariance.py
@@ -23,6 +23,7 @@ def test_tetris_equivariance():
 
 
 def test_tetris_simple_equivariance():
+    # torch.set_default_dtype(torch.float64) # changing this to float64 and making the default dtype 64 doesn't improve the equivariance tolerance
     x, y = tetris()
     # x, y = x[1:], y[1:]  # predict both chiral shapes
 
